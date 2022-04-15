e969edd0d3a9c180da249ae214212969b4a3392a
MaxCount <max.kh002@gmail.com>
2022-04-15 22:32:32 +0300

    yes

---
+++ src/main/java/com/empl/emploemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.empl.emploemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/com/empl/emploemanager/EmploeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empl.emploemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
